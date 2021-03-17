c9bf851254e86f7d34b45c96fda99906dfac26f0
Ann Yesip <50151044+AnnYesip@users.noreply.github.com>
2021-03-17 20:24:54 +0200

    fetchCurrentWeather

--- Weather/Networking/NetworkWeatherManager.swift
+++ Weather/Networking/NetworkWeatherManager.swift
@@ -18,22 +18,16 @@ class NetworkWeatherManager{
     var onCompletion: ((CurrentWeather) -> Void)?
     
     func fetchCurrentWeather(forRequestType requestType: RequestType){
-
+        let urlString: String
         switch requestType {
         case .cityName(city: let city):
-            guard let firstUrl = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiId)&units=metric") else { return  }
-            performRequest(withURLString: firstUrl)
-            
+            urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiId)&units=metric"
             
         case .coordinate(let latitude, let longitude):
-            guard let firstUrl = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiId)&units=metric") else { return  }
-            performRequest(withURLString: firstUrl)
-            
+            urlString =  "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiId)&units=metric"
         }
-    }
-    
-
-    fileprivate func performRequest(withURLString url: URL) {
+        
+        guard let url = URL(string: urlString) else { return }
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
@@ -49,9 +43,5 @@ class NetworkWeatherManager{
             }
         }
         task.resume()
-        
     }
-    
-
-
 }
